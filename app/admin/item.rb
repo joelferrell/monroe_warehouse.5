c3311f7f1f0f0f84b3d58e054c7c4f547cba74c9
Prelang Builder <builder@prelang.com>
2015-04-16 13:58:26 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Item' to allow for create, update

--- app/admin/item.rb
+++ app/admin/item.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Item do
+  permit_params :model, :purchase_date, :description
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
